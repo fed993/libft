6394ab6ea8f32bd1588d0c556099b5183343775e
Fedor Polyanskiy <fpolyans@e1z2r9p26.42.us.org>
2017-09-30 18:23:10 -0700

    first iteration logic for itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: fpolyans <fpolyans@42.us.org>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/28 18:56:51 by fpolyans          #+#    #+#             */
-/*   Updated: 2017/09/28 19:03:28 by fpolyans         ###   ########.fr       */
+/*   Updated: 2017/09/30 18:18:50 by fpolyans         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,25 @@
 
 char	*ft_itoa(int n)
 {
-	
+	int		divisor;
+	char	*nstr;
+
+	divisor = 1000000000;
+	nstr = (char*)malloc((sizeof(char) * 11) + 1);
+	if(nstr)
+		return (NULL);
+	if (n < 0)
+	{
+		*nstr = '-';
+		nstr++;
+	}
+	while (n > 0)
+	{
+		*nstr = (n / divisor) + '0';
+		n = n % divisor;
+		divisor = divisor / 10;
+		nstr++;
+	}
+	*nstr = '\0';
+	return (nstr);
 }
