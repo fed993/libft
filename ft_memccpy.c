bd553a6dacb2a131408d45008e35979379f5580c
Fedor Polyanskiy <fpolyans@e1z2r3p22.42.us.org>
2017-10-10 16:47:56 -0700

    pushing memccpy for debug

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: fpolyans <fpolyans@42.us.org>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/09 13:32:56 by fpolyans          #+#    #+#             */
-/*   Updated: 2017/10/10 15:55:53 by fpolyans         ###   ########.fr       */
+/*   Updated: 2017/10/10 16:39:06 by fpolyans         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,17 +24,4 @@ void	*ft_memccpy(void *str1, const void *str2, int c,  size_t n)
 	}
 	((char*)str1)[i] = ((char*)str2)[i];
 	return (str1);
-}
-
-int		main()
-{
-	char	*str1;
-	char	*str2;
-	char	buf[51];
-
-	str1 = malloc(50);
-	str2 = malloc(50);
-	str1 = "sample text";
-	str2 = ft_memccpy(buf, str1, ' ', 50);
-	write(1, str2, sizeof(str2));
 }
