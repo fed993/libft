7c21c51ab574534e024f5ace53b1e921c5323636
Fed <fed993@gmail.com>
2017-10-14 01:02:35 -0700

    staging ft_memccpy compiled for debug

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fpolyans <fpolyans@42.us.org>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/25 20:14:00 by fpolyans          #+#    #+#             */
+/*   Updated: 2017/10/13 20:20:22 by fpolyans         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
@@ -6,14 +17,17 @@ char	*ft_strtrim(char const *s)
 	char	*str;
 	int		i;
 
-	str = (char*)malloc(ft_strlen(s) * sizeof(char));
+	str = (char*)malloc((ft_strlen(s) * sizeof(char)) + 1);
 	if (str)
 	{
 		i = 0;
 		while (*s)
 		{
 			if (*s != ' ' && *s != '\n' && *s != '\t')
-				str[i++] = *s;
+			{
+				i++;
+				str[i] = *s;
+			}
 			s++;
 		}
 		str[i] = '\0';
